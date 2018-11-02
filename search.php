4cf310f79a12d7482a111a2bcc6a6a1ba6241809
Saleumsack <saleumsack16@gmail.com>
2018-11-02 14:06:26 +0700

    Add Search Function

--- search.php
+++ search.php
@@ -1,15 +1,18 @@
 <?php
 require './db.php';
 
-$idProduct = $_POST['idproduct'];
+if (isset($_POST['search'])) {
+    $word = $_POST['search'];
 
-$deleteQuery = "DELETE FROM tb_product WHERE id=:id";
-$deleteResult = $conn->prepare($deleteQuery);
-$deleteExec = $deleteResult->execute(array(":id" => $idProduct));
+    $searchQuery = "SELECT * FROM tb_product WHERE product_name LIKE :word";
+    $search = '%' . $word . '%';
+    $sql = $conn->prepare($searchQuery);
+    $sql->bindValue(":word", $search);
+    $sql->execute();
 
-$sql = $conn->prepare("SELECT * FROM tb_product;");
-$sql->execute();
-
+} else {
+    $sql = $conn->query("SELECT * FROM tb_product;");
+}
 ?>
 
 <!DOCTYPE html>
@@ -75,49 +78,48 @@ tr:hover {background-color:#f5f5f5;}
   <div class="card">
 
     <h2 style="text-align:center">Search management</h2>
-    <form action="./delete.php" method="POST">
-      <label for="fname">ID Product</label>
-      <input type="test" name="idproduct" id="idproduct" placeholder="You can type anyword that you need to search" required>
-      <input type="submit" value="Search Data" name="delete">
+    <form action="search.php"method="POST">
+      <label for="fname">Product Name or Product Description</label>
+      <input type="test" name="search" id="search" placeholder="You can type anyword that you need to search" required>
+      <input type="submit" value="Search Data" name="submit">
     </form>
 
 
   </div>
   <br>
   <div class="card">
-  <form action="./table.php" method="post">
+
     <table>
       <tr style="background-color: bisque">
         <th>id</th>
         <th>Name</th>
         <th>Price</th>
         <th>Detail</th>
-        <th>Delete</th>
-        <th>Edit</th>
       </tr>
-      <?php
-        while ($res = $sql->fetch(PDO::FETCH_ASSOC)) {
-      ?>
+
+<?php
+$index = 1;
+while ($res = $sql->fetch(PDO::FETCH_ASSOC)) {
+    ?>
       <tr>
-        <td><?echo $res['id']; ?></td>
-       
-        <td><?= $res['product_name']; ?></td>
-        <td><?= $res['product_price']; ?></td>
+        <td><?php echo $index++ ?></td>
+
+        <td><?=$res['product_name'];?></td>
+        <td><?=$res['product_price'];?></td>
         <td><?echo $res['product_detail']; ?></td>
-        <!-- <td><input type="submit" value="Delete" name="delete" onclick="return confirm('Are you sure? to delete id: '+ <? echo $res['id']?>)"></td> -->
-        <td><a href="./table.php?id=<?php echo $res['id']?>" onclick="return confirm('Are you sure? to delete id:' + <?php echo $res['id']?> )">delete</a></td>
-        <td><input type="button" value="Edit"></td>
       </tr>
+
       <?php
-        }
-      ?>
+}
+?>
     </table>
-    </form>
+
   </div>
   <br>
   <div style="text-align: center">
 <a href="./index.php">Home?</a>
 </div>
+
 </body>
 
 </html>
